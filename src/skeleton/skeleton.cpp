5c848b1a4954c9dd5fec55273fe4f63a09d846ac
Cayetano Manchon Pernis <tano@tano.xyz>
2020-06-22 17:18:08 +0000

    Better identation

--- src/skeleton/skeleton.cpp
+++ src/skeleton/skeleton.cpp
@@ -27,24 +27,24 @@ cv::Mat Skeleton::processImage(int width){
 
 	//START CHRONO
 	debug_timer_start(Process_Image)
-	
-	//Generate input blob from image
-	auto inputBlob = cv::dnn::blobFromImage(
-		image, 
-		1.0/255.0, 
-		cv::Size((int)((width*image.cols)/image.rows),width),
-		cv::Scalar(0,0,0),
-		false,
-		false);
+
+		//Generate input blob from image
+		auto inputBlob = cv::dnn::blobFromImage(
+				image,
+				1.0/255.0,
+				cv::Size((int)((width*image.cols)/image.rows),width),
+				cv::Scalar(0,0,0),
+				false,
+				false);
 
 	//Process image
 	neuralNet.setInput(inputBlob);
 	auto netOutputBlob = neuralNet.forward();
-	
+
 	//END CHRONO
 	debug_timer_finish(Process_Image)
 
-	return netOutputBlob;
+		return netOutputBlob;
 }
 
 void Skeleton::extractJoints2(double threshhold=0.1, int width=368){
@@ -62,9 +62,9 @@ void Skeleton::extractJoints2(double threshhold=0.1, int width=368){
 		double x = (double)maxLoc.x/(double)w*(double)image.size().width;
 		double y = (double)maxLoc.y/(double)h*(double)image.size().height;
 		if(maxVal > threshhold){
-			jointsVec[joint]=cv::Point(x,y);	
+			jointsVec[joint]=cv::Point(x,y);
 		}
-	
+
 	}
 }
 
@@ -77,23 +77,26 @@ void Skeleton::extractJoints(double threshhold=0.1, int width=368){
 		cv::Point maxLoc;
 		cv::minMaxLoc(netOutputParts[joint],0,&maxVal,0,&maxLoc);
 		if(maxVal > threshhold){
-			jointsVec[joint]=maxLoc;	
+			jointsVec[joint]=maxLoc;
 		}
-	
+
 	}
 }
 
-Skeleton::Skeleton(std::string_view filename, double threshhold, int width) : image { cv::imread(filename.data(), cv::IMREAD_COLOR)}{
+Skeleton::Skeleton(std::string_view filename, double threshhold, int width)
+	: image { cv::imread(filename.data(), cv::IMREAD_COLOR)}
+{
 	this->jointsVec.resize(Skeleton::model->getMaxJoints());
 	extractJoints2(threshhold,width);
 	paint();
 }
 
-Skeleton::Skeleton(const cv::Mat& img, double threshhold, int width) : image {img}{
-	this->jointsVec.resize(Skeleton::model->getMaxJoints());
-	extractJoints2(threshhold,width);
-	paint();
-}
+Skeleton::Skeleton(const cv::Mat& img, double threshhold, int width)
+	: image {img}{
+		this->jointsVec.resize(Skeleton::model->getMaxJoints());
+		extractJoints2(threshhold,width);
+		paint();
+	}
 
 void Skeleton::show(std::string_view windowName) const{
 	cv::imshow(windowName.data(),image);
@@ -109,22 +112,22 @@ void Skeleton::paint(){
 	for( auto& [j1, j2] : Skeleton::model->getPairs() ){
 		if(jointsVec[j1] && jointsVec[j2]){
 			cv::line(
-				outputFrame, 
-				jointsVec[j1].value(), 
-				jointsVec[j2].value(), 
-				cv::Scalar(255,0,0),
-				5);
+					outputFrame,
+					jointsVec[j1].value(),
+					jointsVec[j2].value(),
+					cv::Scalar(255,0,0),
+					5);
 		}
 	}
 	for(auto& joint : jointsVec){
 		if(joint){
 			cv::circle(
-				outputFrame, 
-				joint.value(),
-				10, 
-				cv::Scalar(0,255,0),
-				-1,
-				cv::LINE_AA);
+					outputFrame,
+					joint.value(),
+					10,
+					cv::Scalar(0,255,0),
+					-1,
+					cv::LINE_AA);
 		}
 	}
 }
